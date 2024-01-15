873eaec7366008153fdbb1c4727c298ac5e7da63
Mikael K Aboagye <mike.amanfo12@gmail.com>
2024-01-14 23:32:05 -0500

    Start console side work

--- VCH/VPCH_Console/Program.cs
+++ VCH/VPCH_Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+
+namespace VPCH
+{
+    public class VPCHOptions
+    {
+        [Option('l', "logsuccess", Required = false, HelpText = "Bool to allow VPCH to create a log file on success.")]
+        public bool LogOnSuccess { get; set; }
+
+        [Option('p', "projectname", Required = true, HelpText = "Name Of the project that you want to generate the vpc file for.")]
+        public required string ProjectName { get; set; }
+
+
+    }
+}
